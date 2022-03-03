fb80bb38564f45d93483d6c9dd93ab5022309291
Rozandi1 <98866052+Rozandi1@users.noreply.github.com>
2022-03-03 20:03:28 +0800

    menyatukan semua page

--- 05-kuliner/script.js
+++ 05-kuliner/script.js
@@ -3,11 +3,14 @@ for (let btnmodal of buttonModal) {
   btnmodal.onclick = function () {
     const cardId = btnmodal.getAttribute("card-id");
     const cardTitle = document.querySelector(`#card-${cardId}-title`);
+
     const modalTitle = document.getElementById("modal-title");
     const cardText = document.querySelector(`#card-${cardId}-text`);
+
     const modalText = document.getElementById("modal-text");
     const cardImg = document.querySelector(`#card-${cardId}-image`);
     const modalImg = document.getElementById("modal-image");
+
     var cardsrc = cardImg.getAttribute("src");
     var modalsrc = modalImg.setAttribute("src", cardsrc);
     modalTitle.innerHTML = cardTitle.innerHTML;
