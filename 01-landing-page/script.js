9cdc78e5b7a094ea7fda12aeab7e74ea40976a6a
Rozandi1 <98866052+Rozandi1@users.noreply.github.com>
2022-03-04 02:18:18 +0800

    memodifikasi designnya

---
+++ 01-landing-page/script.js
@@ -0,0 +1,13 @@
+// const button = document.querySelectorAll(".btn");
+// for (let btnSubmit of button) {
+//   btnSubmit.onclick = function () {
+//     const newsLetter = btnSubmit.getAttribute("newsletter1");
+
+//     const text = document.querySelector(`#exampleFormControlTextarea1-${form - control}`);
+//     const modalText = document.getElementById("modal-text");
+//     var cardsrc = cardImg.getAttribute("src");
+//     var modalsrc = modalImg.setAttribute("src", cardsrc);
+//     modalTitle.innerHTML = newsLetter.innerHTML;
+//     modalText.innerHTML = text.innerHTML;
+//   };
+// }
